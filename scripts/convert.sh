64cb2c476290523167ed301c2ceb706e9c45c87e
justin <justin.ethier@gmail.com>
2012-05-07 18:05:57 -0400

    Added a TODO

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd images/gallery
 rm -f *thumb*
+# TODO: try to crop instead of resize
 find . -type f -name '*.png' | while read filename; do echo "converting ${filename}"; convert -size 128x96 -resize 128x96 +profile '*' "${filename}" "`echo "${filename}" | sed -e 's/\.png$/\.thumb.png/'`"; done
