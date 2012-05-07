322e8a80fa3dfcf37916306f6efc0ba5fe3bc2d7
justin <justin.ethier@gmail.com>
2012-05-07 19:48:59 -0400

    Improved quality of thumbs by generating thumbs using cropped images instead of resized ones.

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd images/gallery
 rm -f *thumb*
-# TODO: try to crop instead of resize
-find . -type f -name '*.png' | while read filename; do echo "converting ${filename}"; convert -size 128x96 -resize 128x96 +profile '*' "${filename}" "`echo "${filename}" | sed -e 's/\.png$/\.thumb.png/'`"; done
+for img in *.png; do
+    filename=${img%.*}
+    convert "$filename.png[128x96+5+5]" "$filename.thumb.png"
+done
