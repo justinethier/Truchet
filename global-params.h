065db6da87b1ef8aa0402a46fa13071d35625323
Justin Ethier <justin.ethier@gmail.com>
2012-05-14 22:46:33 -0400

    Tweaked parameters

--- global-params.h
+++ global-params.h
@@ -1,10 +1,10 @@
 #ifndef __global_params_h_included__
 #define __global_params_h_included__
 
-const int WIDTH = 800;
+const int WIDTH = 860;
 const int HEIGHT= 600;
-const int TILE_WIDTH = 60;
-const int TILE_HEIGHT = 60;
-const int LINE_THICKNESS = 2;
+const int TILE_WIDTH = 100;
+const int TILE_HEIGHT = 100;
+const int LINE_THICKNESS = 20;
 
 #endif
